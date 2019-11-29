67008eee6f69ac1c5d64f6eb715e9efc3f585162
Unknown <jeorch@foxmail.com>
2019-11-29 15:54:52 +0800

    暂时的main

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/PharbersDeveloper/bp-go-lib/env"
+	"github.com/PharbersDeveloper/bp-jobs-observer/observers"
+	"os"
+)
+
+func main() {
+	setEnv()
+
+	bfjo := observers.BpFileJobsObserver{
+		Id:         "test007",
+		DBHost:     "59.110.31.50",
+		DBPort:     "5555",
+		Database:   "pharbers-sandbox-600",
+		Collection: "assets",
+		Conditions: map[string]interface{}{
+			"$and": []map[string]interface{}{
+				map[string]interface{}{"file": map[string]interface{}{"$exists": true, "$ne": ""}},
+				map[string]interface{}{"isNewVersion": true},
+				map[string]interface{}{"dfs": map[string]interface{}{"$exists": true, "$size": 0}},
+			},
+		},
+		ParallelNumber:         1,
+		SingleJobTimeoutSecond: 10,
+		ScheduleDurationSecond: 60,
+		RequestTopic:           "test006",
+		ResponseTopic:          "test007",
+	}
+	bfjo.Open()
+	bfjo.Exec()
+}
+
+func setEnv() {
+	//项目范围内的环境变量
+	_ = os.Setenv(env.ProjectName, "bp-jobs-observer")
+
+	//log
+	_ = os.Setenv(env.LogTimeFormat, "2006-01-02 15:04:05")
+	_ = os.Setenv(env.LogOutput, "console")
+	//_ = os.Setenv(env.LogOutput, "./tmp/bp-jobs-observer.log")
+	_ = os.Setenv(env.LogLevel, "info")
+
+	//kafka
+	_ = os.Setenv(env.KafkaConfigPath, "resources/kafka_config.json")
+	_ = os.Setenv(env.KafkaSchemaRegistryUrl, "http://123.56.179.133:8081")
+}
